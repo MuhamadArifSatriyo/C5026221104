ce3e0898367bfe752970900763e533ce1d9a7a21
MuhamadArifSatriyo <muhamad.sen3818@gmail.com>
2023-11-23 14:55:11 +0700

    Pertemuan tgl 23 Nop 2023

--- routes/web.php
+++ routes/web.php
@@ -84,3 +84,11 @@ Route::get('/master/tentang', function () {
 Route::get('/master/kontak', function () {
 	return view('kontak');
 });
+
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
